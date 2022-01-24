17e8c702f751ce919105ec9c22dffcf13342d42f
BispoGitHub <98343220+BispoGitHub@users.noreply.github.com>
2022-01-24 16:14:04 -0300

    Adição do helloWorld.py

---
+++ helloWorld.py
@@ -0,0 +1,10 @@
+# # # # # # # # # # # # # # # # # # # # # # #
+# Autor: Marcus Vinicius Bispo Barbosa      #
+# Data de criação: 24/01/2022               #
+#                                           #
+# Descrição: Este programa foi feito apenas #
+# para imprimir uma mensagem de "Hello      #
+# world" na tela do terminal.               #
+# # # # # # # # # # # # # # # # # # # # # # # 
+
+print("Hello, world")
