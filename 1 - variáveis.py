f5667d03dee73d56a97051c988be25372fbb4406
BispoGitHub <98343220+BispoGitHub@users.noreply.github.com>
2022-01-24 20:48:43 -0300

    Adição do variáveis.py

---
+++ 1 - variáveis.py
@@ -0,0 +1,12 @@
+# # # # # # # # # # # # # # # # # # # # # # #
+# Autor: Marcus Vinicius Bispo Barbosa      #
+# Data de criação: 24/01/2022               #
+#                                           #
+# Descrição: Este programa foi feito apenas #
+# para exemplificar a atribuição de valores #
+# em variáveis.                             #
+# # # # # # # # # # # # # # # # # # # # # # #
+
+texto = "Pizza"
+inteiro = 11
+pontoFlutuante = 3.14
