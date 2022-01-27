f2994e3161516c99e7210e321dd9db89eb05a6cb
Marcus Vinicius Bispo Barbosa <github.bispo@gmail.com>
2022-01-27 16:11:05 -0300

    Adição do tuplas.py

---
+++ 7_tuplas.py
@@ -0,0 +1,16 @@
+# # # # # # # # # # # # # # # # # # # # # # #
+# Autor: Marcus Vinicius Bispo Barbosa      #
+# Data de criação: 27/01/2022               #
+#                                           #
+# Descrição: Este programa foi feito para   #
+# apresentar os conceitos em torno das      #
+# tuplas em python.                         #
+# # # # # # # # # # # # # # # # # # # # # # #
+
+tupla = (1, 2, 3)
+
+print(tupla[0]) # 1
+
+tupla.clear()
+
+print(tupla)    # ()
