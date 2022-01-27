ef1ddf7c9a8aeaa62e7920bdd3bd21550fa6c172
Marcus Vinicius Bispo Barbosa <github.bispo@gmail.com>
2022-01-27 15:01:27 -0300

    Alteração dos nomes dos arquivos

---
+++ 4_repeticao.py
@@ -0,0 +1,16 @@
+# # # # # # # # # # # # # # # # # # # # # # #
+# Autor: Marcus Vinicius Bispo Barbosa      #
+# Data de criação: 26/01/2022               #
+#                                           #
+# Descrição: Este programa foi feito para   #
+# exibir conceitos relacionados a           #
+# estruturas de repetição                   #
+# # # # # # # # # # # # # # # # # # # # # # #
+
+for i in range(10):
+    print(i)
+
+i = 0
+
+while i < 10:
+    print(i)
