bdc5e5b3dbd430ab5a49c94711d9c184fe955286
Marcus Vinicius Bispo Barbosa <github.bispo@gmail.com>
2022-01-27 15:42:53 -0300

    Adição do funcoes.py

---
+++ 5_funcoes.py
@@ -0,0 +1,14 @@
+# # # # # # # # # # # # # # # # # # # # # # #
+# Autor: Marcus Vinicius Bispo Barbosa      #
+# Data de criação: 27/01/2022               #
+#                                           #
+# Descrição: Este programa foi feito para   #
+# exibir como funcionam as funções em       #
+# python.                                   #
+# # # # # # # # # # # # # # # # # # # # # # #
+
+def soma(param1, param2):
+    result = param1 + param2
+    return result
+
+print(soma(3, 4)) # 7
